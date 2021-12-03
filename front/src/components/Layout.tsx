0977031c837658b8f780889827463d0a2c0ceb2f
Cameron Jones <camandrewjones@gmail.com>
2021-12-03 13:35:06 +0200

    Layout in working order

--- front/src/components/Layout.tsx
+++ front/src/components/Layout.tsx
@@ -1,10 +1,174 @@
-import { Outlet } from 'react-router-dom'
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar'
+import {
+    Drawer as MuiDrawer,
+    AppBar as MuiAppBar,
+    styled,
+    Toolbar,
+    IconButton,
+    Typography,
+    Badge,
+    Divider,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Paper,
+    Container,
+    Grid,
+    ListItemButton
+} from '@mui/material'
+import { ChevronLeft, Menu, Notifications, MenuBook, EmojiEvents } from '@mui/icons-material'
+import { Box } from '@mui/system'
 
-export default function Layout() {
+export type RouteDefinition = {
+    display: string
+    link: string
+    icon: React.ReactElement
+}
+
+export const sideBarRoutes: RouteDefinition[] = [
+    { display: 'My Club', link: '/club', icon: <MenuBook /> },
+    { display: 'Awards', link: '/awards', icon: <EmojiEvents /> }
+]
+
+const drawerWidth: number = 240
+
+interface AppBarProps extends MuiAppBarProps {
+    open?: boolean
+}
+
+const AppBar = styled(MuiAppBar, {
+    shouldForwardProp: prop => prop !== 'open'
+})<AppBarProps>(({ theme, open }) => ({
+    zIndex: theme.zIndex.drawer + 1,
+    transition: theme.transitions.create(['width', 'margin'], {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen
+    }),
+    ...(open && {
+        marginLeft: drawerWidth,
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['width', 'margin'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen
+        })
+    })
+}))
+
+const Drawer = styled(MuiDrawer, { shouldForwardProp: prop => prop !== 'open' })(
+    ({ theme, open }) => ({
+        '& .MuiDrawer-paper': {
+            position: 'relative',
+            whiteSpace: 'nowrap',
+            width: drawerWidth,
+            transition: theme.transitions.create('width', {
+                easing: theme.transitions.easing.sharp,
+                duration: theme.transitions.duration.enteringScreen
+            }),
+            boxSizing: 'border-box',
+            ...(!open && {
+                overflowX: 'hidden',
+                transition: theme.transitions.create('width', {
+                    easing: theme.transitions.easing.sharp,
+                    duration: theme.transitions.duration.leavingScreen
+                }),
+                width: theme.spacing(7),
+                [theme.breakpoints.up('sm')]: {
+                    width: theme.spacing(9)
+                }
+            })
+        }
+    })
+)
+
+function DashboardContent() {
+    const [open, setOpen] = React.useState(true)
+    const toggleDrawer = () => {
+        setOpen(!open)
+    }
+
     return (
-        <>
-            <p>Layout!</p>
-            <Outlet />
-        </>
+        <Box sx={{ display: 'flex' }}>
+            <AppBar position="absolute" open={open}>
+                <Toolbar
+                    sx={{
+                        pr: '24px' // keep right padding when drawer closed
+                    }}
+                >
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer}
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' })
+                        }}
+                    >
+                        <Menu />
+                    </IconButton>
+                    <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                    >
+                        Dashboard
+                    </Typography>
+                    <IconButton color="inherit">
+                        <Badge badgeContent={4} color="secondary">
+                            <Notifications />
+                        </Badge>
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
+            <Drawer variant="permanent" open={open}>
+                <Toolbar
+                    sx={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'flex-end',
+                        px: [1]
+                    }}
+                >
+                    <IconButton onClick={toggleDrawer}>
+                        <ChevronLeft />
+                    </IconButton>
+                </Toolbar>
+                <Divider />
+                <List>
+                    {sideBarRoutes.map(r => (
+                        <ListItem button>
+                            <ListItemIcon>{r.icon}</ListItemIcon>
+                            <ListItemText primary={r.display} />
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+                {/* <List>{secondaryListItems}</List> */}
+            </Drawer>
+            <Box
+                component="main"
+                sx={{
+                    backgroundColor: theme =>
+                        theme.palette.mode === 'light'
+                            ? theme.palette.grey[100]
+                            : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: '100vh',
+                    overflow: 'auto'
+                }}
+            >
+                <Toolbar />
+                <Outlet />
+            </Box>
+        </Box>
     )
+}
+
+export default function Dashboard() {
+    return <DashboardContent />
 }
--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material'
+import { Box, Button, Container, Grid, Paper, Toolbar } from '@mui/material'
 import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../context/auth_context'
@@ -11,18 +11,58 @@ export default function Home() {
     }, [auth.user])
 
     return (
-        <>
-            <Button
-                onClick={() => {
-                    if (auth.logout) {
-                        auth.logout()
-                    }
-                }}
-            >
-                Logout
-            </Button>
-            <Link to={'/login'}>Login</Link>
-            <p>Home!</p>
-        </>
+        <Box
+            component="main"
+            sx={{
+                backgroundColor: (theme: any) =>
+                    theme.palette.mode === 'light'
+                        ? theme.palette.grey[100]
+                        : theme.palette.grey[900],
+                flexGrow: 1,
+                height: '100vh',
+                overflow: 'auto'
+            }}
+        >
+            <Toolbar />
+            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                <Grid container spacing={3}>
+                    {/* Chart */}
+                    <Grid item xs={12} md={8} lg={9}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 240
+                            }}
+                        >
+                            {/* <Chart /> */}
+                            Chart
+                        </Paper>
+                    </Grid>
+                    {/* Recent Deposits */}
+                    <Grid item xs={12} md={4} lg={3}>
+                        <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 240
+                            }}
+                        >
+                            {/* <Deposits /> */}
+                            Deposits
+                        </Paper>
+                    </Grid>
+                    {/* Recent Orders */}
+                    <Grid item xs={12}>
+                        <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                            {/* <Orders /> */}
+                            Orders
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
     )
 }
